14f9c5cb3aa302dbd3a66662d41cbdf3157e954b
Mahdi El Dirani <diranimahdi2002@gmail.com>
2024-03-03 01:38:24 +0100

    l3eeb

--- src/components/home/welcome.js
+++ src/components/home/welcome.js
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import arrow from "../../imgs/down.png"
 import welcome from "../../imgs/welcome3.jpg"
 import mobile from "../../imgs/welcomeMobile.jpg"
+import { useTranslation } from "../../context/translation";
 export default function Welcome(){
-    const [size,setSize]=useState(null)
+    const [size,setSize]=useState(null);
+    const {t}=useTranslation();
     useEffect(() => {
         const handleResize = () => {
             setSize(window.innerWidth);
@@ -28,8 +30,8 @@ export default function Welcome(){
         }} 
         className="welcome flex justify-center items-center pl-5 h-screen ">
             <div className="flex flex-col justify-center">
-                <p className="title text-white text-4xl">Dive into a world of enjoyment</p>
-                <p className="text-[#d1dde7] text-2xl">and challenge yourself with our selection of engaging games!</p>
+                <p className="title text-white text-4xl">{t("welcome-sentence1")}</p>
+                <p className="text-[#d1dde7] text-2xl">{t("welcome-sentence2")}</p>
             </div>
             <img alt="welcome" src={arrow} className="absolute bottom-3"></img>
         </div>
