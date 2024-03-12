271717b39c894d788e694c258d3c5501c7bc6fcc
Mahdi El Dirani <diranimahdi2002@gmail.com>
2024-03-12 09:39:55 +0100

    abo njeeb

--- src/components/games/tictac/winnerTic.js
+++ src/components/games/tictac/winnerTic.js
@@ -1,24 +1,40 @@
-import { useTranslation } from "../../../context/translation"
-
+import React, { useState, useEffect } from 'react';
+import { useTranslation } from "../../../context/translation";
+import abo from "../../../imgs/abo.mp4"
 export default function WinnerTic({ winner, playagain }) {
-    const { t, RTL} = useTranslation()
+    const { t, RTL } = useTranslation();
+    const [showVideo, setShowVideo] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowVideo(false);
+        }, 4000);
+
+        return () => clearTimeout(timer);
+    }, []);
 
     return (
         <>
+            {showVideo && (
+                <div dir={RTL ? "rtl" : "ltr"} className={`text-5xl absolute text-white count-overlay flex flex-col justify-center items-center top-0 left-0 w-full h-full`}>
 
-            <div dir={RTL ? "rtl" : "ltr"} className={`text-5xl absolute text-white count-overlay flex flex-col justify-center items-center top-0 left-0 w-full h-full`}>
-                {winner} {winner === t("tic-draw") ? "" : t("tic-wins")}
+                    <video autoPlay>
+                        <source src={abo} type="video/mp4" />
+                    </video>
+                </div>
+            )}
+            {!showVideo && (
+                <div dir={RTL ? "rtl" : "ltr"} className={`text-5xl absolute text-white count-overlay flex flex-col justify-center items-center top-0 left-0 w-full h-full`}>
+                    {winner} {winner === t("tic-draw") ? "" : t("tic-wins")}
 
-
-                <button
-                    onClick={() => playagain()}
-                    className={`rounded-xl mt-10 text-xs p-2 w-26 font-bold text-white border-2 justify-self-end  border-[#b9004e]`}>
-                    {t("tic-again")}
-                </button>
-
-
-            </div>
-
+                    <button
+                        onClick={() => playagain()}
+                        className={`rounded-xl mt-10 text-xs p-2 w-26 font-bold text-white border-2 justify-self-end  border-[#b9004e]`}
+                    >
+                        {t("tic-again")}
+                    </button>
+                </div>
+            )}
         </>
-    )
+    );
 }
